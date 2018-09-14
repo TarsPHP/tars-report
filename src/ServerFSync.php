2d83e0a18f130c2be225dfa2111be51b34781eea
p_tedcliang <liangchen@yuewen.com>
2018-09-14 22:23:48 +0800

    remove recv, no need

--- src/ServerFSync.php
+++ src/ServerFSync.php
@@ -36,55 +36,19 @@ class ServerFSync
             $tarsRequestBuf = \TUPAPI::encode($iVersion, $iRequestId, $servantName,
                 $funcName, $cPacketType, $iMessageType, $tarsTimeout, $contexts, $statuses, $encodeBufs);
 
-            $time = microtime(true);
-
             $client = new \swoole_client(SWOOLE_SOCK_TCP | SWOOLE_KEEP);
 
             $timeout = 2;
             if (!$client->connect($this->_ip, $this->_port, $timeout)) {
-                throw new \Exception();
+                return 0;
             }
 
             if (!$client->send($tarsRequestBuf)) {
                 $client->close();
-                throw new \Exception();
+                return 0;
             }
 
-            $totalLen = 0;
-            $tarsResponseBuf = null;
-            while (true) {
-                if (microtime(true) - $time > $timeout) {
-                    $client->close();
-                    throw new \Exception();
-                }
-
-                //读取最多32M的数据
-                $data = $client->recv(65535);
-
-                if (empty($data)) {
-                    $client->close();
-                    // 已经断开连接
-                    throw new \Exception();
-                } else {
-                    //第一个包
-                    if ($tarsResponseBuf === null) {
-                        $tarsResponseBuf = $data;
-
-                        //在这里从第一个包中获取总包长
-                        $list = unpack('Nlen', substr($data, 0, 4));
-                        $totalLen = $list['len'];
-                    } else {
-                        $tarsResponseBuf .= $data;
-                    }
-
-                    //check if all package is receved
-                    if (strlen($tarsResponseBuf) >= $totalLen) {
-                        $client->close();
-
-                        return 0;
-                    }
-                }
-            }
+            return 0;
         } catch (\Exception $e) {
             return -1;
         }
